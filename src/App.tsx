9bfe851a6cb97286412d73155ac8ed7980f40f9b
RustemAbdulaev <rustem.abdulaev@gmail.com>
2023-09-11 10:45:54 +0300

    Input

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
-import React from 'react';
+import React, {useState} from 'react';
+import Input from "./components/Input";
+const App = () => {
+    const [height, setHeight] = useState<string>('');
+    const [radius, setRadius] = useState<string>('');
+    const [segments, setSegments] = useState<string>('');
 
 
-function App() {
-  return (
-      <div>
-     </div>
-  );
-}
 
+    return (
+        <div className="bg-black">
+                <h1 className='text-center font-bold text-5xl'>Create Own Cone!</h1>
+                <div className="flex justify-center mt-5">
+                    <Input value={height} setValue={setHeight} label="Cone Height" />
+                    <Input value={radius} setValue={setRadius} label="Cone Radius"/>
+                    <Input value={segments} setValue={setSegments} label="Cone Segments"/>
+                </div>
+        </div>
+    );
+};
 export default App;
---
+++ src/components/Input.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+
+interface IProps{
+    value:string
+    label: string
+    setValue:  React.Dispatch<React.SetStateAction<string>>
+}
+const Input = ({value,setValue,label}:IProps) => {
+    return (
+        <div className="flex flex-col items-center">
+            <label htmlFor='idInput' className="text-right mb-2 font-semibold text-1xl">{label}
+            </label>
+            <input
+                className="text-center bg-gray-500"
+                id="idInput"
+                type="text"
+                value={value}
+                onChange={e => {
+                    const val = e.target.value;
+                    // Проверка на валидность числа
+                    if (/^-?\d*\.?\d*$/.test(val) || val === '') {
+                        setValue(val);
+                    }}}
+                onKeyDown={(e)=>{
+                    if(e.key=== 'Escape'){
+                        setValue('')
+                    }
+                }}/>
+
+
+        </div>
+    );
+};
+
+export default Input;
+
+
